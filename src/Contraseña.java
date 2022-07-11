36d5672e934f46804e3625284bb99c7bb3444fac
KevinxDx3 <85362446+KevinxDx3@users.noreply.github.com>
2022-07-11 08:23:27 -0500

    cambio2

--- src/Contraseña.java
+++ src/Contraseña.java
@@ -2,37 +2,37 @@ package src;
 
 public class Contraseña {
 
-    public double contraseña1;
-    public double contraseña2;
-    public double contraseña3;
+    public int contraseña1;
+    public int contraseña2;
+    public int contraseña3;
 
-    public Contraseña(double contraseña1, double contraseña2, double contraseña3) {
+    public Contraseña(int contraseña1, int contraseña2, int contraseña3) {
         this.contraseña1 = contraseña1;
         this.contraseña2 = contraseña2;
         this.contraseña3 = contraseña3;
     }
 
-    public double getContraseña1() {
+    public int getContraseña1() {
         return contraseña1;
     }
 
-    public void setContraseña1(double contraseña1) {
+    public void setContraseña1(int contraseña1) {
         this.contraseña1 = contraseña1;
     }
 
-    public double getContraseña2() {
+    public int getContraseña2() {
         return contraseña2;
     }
 
-    public void setContraseña2(double contraseña2) {
+    public void setContraseña2(int contraseña2) {
         this.contraseña2 = contraseña2;
     }
 
-    public double getContraseña3() {
+    public int getContraseña3() {
         return contraseña3;
     }
 
-    public void setContraseña3(double contraseña3) {
+    public void setContraseña3(int contraseña3) {
         this.contraseña3 = contraseña3;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,41 @@ public class Main {
         static String usur1="Kevin";
         static String usur2="Jose";
         static String usur3="Jorge";
+
+        int contra;
+        static int contra1=5002;
+        static int contra2=1234;
+        static int contra3=5006;
+
+
         public static void main (String args []) {
 
             Scanner teclado=new Scanner(System.in);
 
             Usuarios usuario= new Usuarios(usur1,usur2,usur3);
-            Contraseña contraseña= new Contraseña(5002,1234,5006);
+            Contraseña contraseña= new Contraseña(contra1,contra2,contra3);
 
             System.out.println("Ingrese su nombre de usuario: ");
             String user = teclado.next();
 
             if (user==usur1 || user==usur2 || user==usur3){
 
+                System.out.println("Ingrese la contraseña: ");
+                int contra = teclado.nextInt();
+
+                if (contra==contra1 || contra==contra2 || contra==contra3){
+
+                    System.out.println("Desea ingresar o retirar dinero? ");
+                    
+
+                }else {
+                    System.out.println("Contraseña mal ingresada, verifique en el archivo readme");
+                }
 
 
             }else{
                 System.out.println("Usuario incorrecto Revise el archivo readme");
             }
-
-
-
-
-
-
-
-
-
 
 
 
