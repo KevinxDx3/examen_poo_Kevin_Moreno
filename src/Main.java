885051bdbcfbd63d43cbac631c4a4041cc9cfa74
KevinxDx3 <85362446+KevinxDx3@users.noreply.github.com>
2022-07-11 08:47:19 -0500

    cambio 2

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,17 @@ public class Main {
         static int contra2=1234;
         static int contra3=5006;
 
+        int op;
 
-        public static void main (String args []) {
+         double dinero1=100;
+         double dinero2=100;
+         double dinero3=100;
+
+        double deposito;
+        double retiro;
+
+
+        public void main(String args[]) {
 
             Scanner teclado=new Scanner(System.in);
 
@@ -25,27 +34,143 @@ public class Main {
             System.out.println("Ingrese su nombre de usuario: ");
             String user = teclado.next();
 
-            if (user==usur1 || user==usur2 || user==usur3){
+            if (user==usur1){
 
-                System.out.println("Ingrese la contraseña: ");
+                System.out.println("Ingrese su contraseña: ");
                 int contra = teclado.nextInt();
 
-                if (contra==contra1 || contra==contra2 || contra==contra3){
+                if (contra==contra1){
 
-                    System.out.println("Desea ingresar o retirar dinero? ");
-                    
+                    System.out.println("1.- Retirar");
+                    System.out.println("2.- Depositar");
+                    System.out.println("3.- Salir");
+
+                    System.out.printf("Seleccione una opcion:");
+                    int op = teclado.nextInt();
+
+                    switch (op){
+                        case 1:
+                            System.out.println("Cuanto desea retirar? ");
+                            double retiro = teclado.nextDouble();
+
+                            System.out.println("Su deposito fue hecho con exito");
+
+                            break;
+                        case 2:
+                            System.out.println("Cuanto desea depositar: ");
+                            double deposito = teclado.nextDouble();
+
+                            System.out.println("Su deposito fue hecho con exito");
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            System.out.println("Solo numeros del 1 al 3");
+
+
+                    }
+
 
                 }else {
-                    System.out.println("Contraseña mal ingresada, verifique en el archivo readme");
+                    System.out.printf("Ingreso mal de contraseña, verifique el archivo readme");
                 }
 
 
+            }else if (user==usur2){
+
+
+                System.out.println("Ingrese su contraseña: ");
+                int contra = teclado.nextInt();
+
+                if (contra==contra2){
+
+                    System.out.println("1.- Retirar");
+                    System.out.println("2.- Depositar");
+                    System.out.println("3.- Salir");
+
+                    System.out.printf("Seleccione una opcion:");
+                    int op = teclado.nextInt();
+
+                    switch (op){
+                        case 1:
+                            System.out.println("Cuanto desea retirar? ");
+                            double retiro = teclado.nextDouble();
+
+                            System.out.println("Su deposito fue hecho con exito");
+
+                            break;
+                        case 2:
+                            System.out.println("Cuanto desea depositar: ");
+                            double deposito = teclado.nextDouble();
+
+                            System.out.println("Su deposito fue hecho con exito");
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            System.out.println("Solo numeros del 1 al 3");
+
+
+                    }
+
+
+                }else {
+                    System.out.printf("Ingreso mal de contraseña, verifique el archivo readme");
+                }
+
+
+
+
+
+
+            }else if (user==usur3){
+
+                System.out.println("Ingrese su contraseña: ");
+                int contra = teclado.nextInt();
+
+                if (contra==contra1){
+
+                    System.out.println("1.- Retirar");
+                    System.out.println("2.- Depositar");
+                    System.out.println("3.- Salir");
+
+                    System.out.printf("Seleccione una opcion:");
+                    int op = teclado.nextInt();
+
+                    switch (op){
+                        case 1:
+                            System.out.println("Cuanto desea retirar? ");
+                            double retiro = teclado.nextDouble();
+
+                            System.out.println("Su deposito fue hecho con exito");
+
+                            break;
+                        case 2:
+                            System.out.println("Cuanto desea depositar: ");
+                            double deposito = teclado.nextDouble();
+
+                            System.out.println("Su deposito fue hecho con exito");
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            System.out.println("Solo numeros del 1 al 3");
+
+
+                    }
+
+
+                }else {
+                    System.out.printf("Ingreso mal de contraseña, verifique el archivo readme");
+                }
+
+
+
             }else{
-                System.out.println("Usuario incorrecto Revise el archivo readme");
+                System.out.println("Ingreso mal el usuario, verifique el archivo readme");
             }
 
 
-
-    }
+        }
 
 }
